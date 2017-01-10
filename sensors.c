c12cbaaed19ff7b0af6dab57eaa0f0177ca91568
ilkin94 <huseynzade94@gmail.com>
2017-01-10 16:35:31 +0100

    Update sensors.c

--- sensors.c
+++ sensors.c
@@ -12,10 +12,22 @@ This function returns the values of the sensors in an array.
 get_sensors_value()
 {
  get_temp();
- light = getLightSensorValue(1.5881,40.157,PHIDGET3V_2 ); 
+ getLightSensorValue(1.5881,40.157,PHIDGET3V_2 ); 
 }
+
 static void get_temp()
 {
+  	int16_t 
+	raw,    // Raw data as received from the sensor. 
+	tempint,  // Integer part of the temperature reading. 		
+	sign;   // Used to evaluate negative temperature readings. 	
+	
+	uint16_t 
+	tempfrac, // Decimal part of the temperature reading. 	
+	absraw;   // Absolute value of raw data. 	
+	char 	
+	minus = ' ';  // Used to print negative temperatures.
+	
   raw = tmp102_read_temp_raw();
 		// Now we have to process the raw data and print it:
 		sign = 1;
@@ -27,17 +39,21 @@ static void get_temp()
 		  sign = -1;
 		}
 		// Extract the integer part:
-		tempint  = (absraw >> 8) * sign;
+		tempint= (absraw >> 8) * sign;
+		message.temp_sensor_reading_int = tempint;
 		// Extract the fractional part in 1/10000 of degree:
 		//tempfrac = ((absraw >> 4) % 16) * 625;
 		// Obtain the sign:
 		if((tempint == 0) & (sign == -1)){
-		  minus = '-';
+		  	minus = '-';
+		  	message.temp_sensor_reading_sign = minus;
+			
 		} else {
-		  minus = ' ';
+		  	minus = ' ';
+		  	message.temp_sensor_reading_sign = minus;
 		}  
 }
-static int getLightSensorValue(float m, float b, uint8_t phidget_input){
+static void getLightSensorValue(float m, float b, uint8_t phidget_input){
 	//Read voltage from the phidget interface
 	float voltage;
 	float SensorValue;
@@ -47,8 +63,9 @@ static int getLightSensorValue(float m, float b, uint8_t phidget_input){
 	//Convert the voltage in lux with the provided formula
 	SensorValue = voltage/4.096;
 	lux = (int)(m * SensorValue + b);
-	//Return the value of the light with maximum value equal to 1000
-	if (lux > 1000)
-		lux = 1000;
-	return lux;
+	//Return the value of the light in boolan whether it is night or noon
+	if (lux < 400)
+		message.light_sensor_reading==FALSE;
+	else 
+		message.light_sensor_reading==TRUE;
 }
